0f3351e85c5d79086306a9673b7ecc19faacb457
Giancarlo Sena Martins Junior <108488305+GiancarloJr@users.noreply.github.com>
2023-06-21 17:57:23 -0300

    update component

--- src/app/components/tablediretivas/tablediretivas.component.ts
+++ src/app/components/tablediretivas/tablediretivas.component.ts
@@ -20,12 +20,10 @@ export class DocTableDiretivasComponent implements OnInit {
 
   //#region Properties/Inputs/ViewChild
 
-  @Input() nameComponent!: string;
+  @Input() nameComponent: string;
 
-  @Input() sectionType!: string;
-
-
-  private formDataComponent!: FormGroup;
+  //NOME SEÇÃO (INPUT,OUTPUT,VIEWCHILD)
+  @Input() sectionType: string;
 
   public formData!: FormGroup;
 
@@ -34,8 +32,6 @@ export class DocTableDiretivasComponent implements OnInit {
   public disabledButtonEdit: boolean = false;
   public disabledButtonSave: boolean = true;
   public disabledButtonAddTable: boolean = false;
-
-  public teste: boolean = true;
 
   public disableTable: boolean = true;
 
@@ -54,13 +50,13 @@ export class DocTableDiretivasComponent implements OnInit {
   ngOnInit(): void {
     this.cadastroService.getComponents(this.returnPathComponent()).subscribe(response => {
       let dataComponente = response.filter(componente => componente.Componente == this.nameComponent).at(0);
-      if (dataComponente!.Dados.filter(decorator => decorator.Secao == this.sectionType).length > 0) {
+      if (dataComponente.Dados.filter(decorator => decorator.Secao == this.sectionType).length > 0) {
         this.disableTable = false;
       }
     });
     this.createForm();
     this.initializeData();
-		(<FormArray>this.formData.get('Dados')).controls.forEach(value => value.disable());
+    (<FormArray>this.formData.get('Dados')).controls.forEach(value => value.disable());
   }
 
   //#endregion
@@ -76,13 +72,12 @@ export class DocTableDiretivasComponent implements OnInit {
   }
 
   buttonEditRow(): void {
-
     this.disabledButtonSave ? this.disabledButtonSave = false : this.disabledButtonSave = true;
     this.disabledButtonEdit ? this.disabledButtonEdit = false : this.disabledButtonEdit = true;
     this.disabledButtonAddRow ? this.disabledButtonAddRow = false : this.disabledButtonAddRow = true;
     this.disabledButtonExclude ? this.disabledButtonExclude = false : this.disabledButtonExclude = true;
     this.disabledButtonAddTable ? this.disabledButtonAddTable = false : this.disabledButtonAddTable = true;
-    this.disableInput();
+    this.enableInput();
   }
 
   buttonExcludeRow(): void {
@@ -111,61 +106,21 @@ export class DocTableDiretivasComponent implements OnInit {
       Componente: [''],
       Dados: this._formBuilder.array([], [Validators.required])
     });
-    this.formDataComponent = this._formBuilder.group({
-      Id: [''],
-      ComponenteId: ['',],
-      Descricao: [{value: '', disabled: false}, Validators.required],
-      Nome: [{value: '', disabled: false}, Validators.required],
-      SecaoId: [''],
-      Secao: ['']
-    });
   }
 
   private createFormGroupDados(dado: Dados): FormGroup {
     return this._formBuilder.group({
       Id: [dado.Id],
       ComponenteId: [dado.ComponenteId],
-      Descricao: [{value: dado.Descricao, disabled: true}, [Validators.required]],
-      Nome: [{value: dado.Nome, disabled: true}, [Validators.required]],
+      Descricao: [{ value: dado.Descricao, disabled: true }, [Validators.required]],
+      Nome: [{ value: dado.Nome, disabled: true }, [Validators.required]],
       SecaoId: [dado.SecaoId],
       Secao: [dado.Secao]
     })
   }
 
-  private orderDadosById(dados: Dados[]): Dados[] {
-    return dados.sort(
-      function (a, b) {
-        return a.Id - b.Id;
-      });
-  }
-
-  public disableInput(): void {
-      (<FormArray>this.formData.get('Dados')).controls.forEach(value => value.disable());
-  }
-
-  public enableInput(): void {
-      return (<FormArray>this.formData.get('Dados')).controls.forEach(value => value.enable());
-  }
-
-  private filterComponente(data: Componente[]): Componente {
-    return data.filter(element => element.Componente == this.nameComponent).at(0)!;
-  }
-
-  private removeItemFromDecoratorsArray(data: Componente, indexForm: number): void {
-    let indexToRemove = data.Dados.findIndex(element => element.Id == indexForm);
-    data.Dados.splice(indexToRemove, 1);
-  }
-
-  private returnPathComponent(): string {
-    return "Componentes";
-  }
-
-  //#endregion
-
-  //#region Public Methods
-
-  // Montagem do(s) ARRAY(s) com as informações do componente
-  public initializeData(): void {
+  //Montagem do(s) ARRAY(s) com as informações do componente
+  private initializeData(): void {
     let dados = this.formData.get('Dados') as FormArray;
     this.cadastroService.getComponents(this.returnPathComponent()).subscribe(response => {
       let dadosComponente: Componente = this.filterComponente(response);
@@ -178,6 +133,38 @@ export class DocTableDiretivasComponent implements OnInit {
       });
     })
   }
+
+  private orderDadosById(dados: Dados[]): Dados[] {
+    return dados.sort(
+      function (a, b) {
+        return a.Id - b.Id;
+      });
+  }
+
+  private disableInput(): void {
+    (<FormArray>this.formData.get('Dados')).controls.forEach(value => value.disable());
+  }
+
+  private enableInput(): void {
+    (<FormArray>this.formData.get('Dados')).controls.forEach(value => value.enable());
+  }
+
+  private filterComponente(data: Componente[]): Componente {
+    return data.filter(element => element.Componente == this.nameComponent).at(0);
+  }
+
+  // private removeItemFromDecoratorsArray(data: Componente, indexForm: number): void {
+  //   let indexToRemove = data.Dados.findIndex(element => element.Id == indexForm);
+  //   data.Dados.splice(indexToRemove, 1);
+  // }
+
+  private returnPathComponent(): string {
+    return "Componentes";
+  }
+
+  //#endregion
+
+  //#region Public Methods  
 
   public getDadosFormArray() {
     return (<FormArray>this.formData.get('Dados')).controls
@@ -202,10 +189,10 @@ export class DocTableDiretivasComponent implements OnInit {
   // Metodo para adicionar linha e gerar novo Id no banco
   public addRow(): void {
     this.buttonAddRow();
-    const dados = this.formData.get('Dados') as FormArray;
+    const dados = this.formData.get('Dados') as FormArray;    
     this.cadastroService.getComponents(this.returnPathComponent()).subscribe(response => {
       let componente: Componente = this.filterComponente(response);
-      const generatedId = componente.Dados.reverse().at(0)!.Id + 1;
+      const generatedId = componente.Dados.reverse().at(0).Id + 1;
       dados.push(this._formBuilder.group({
         Id: [generatedId],
         ComponenteId: [componente.ComponenteId],
@@ -213,41 +200,32 @@ export class DocTableDiretivasComponent implements OnInit {
         Nome: ['', [Validators.required]],
         SecaoId: [1],
         Secao: [this.sectionType]
-      }))
+      }));
     });
   }
 
   // Metodo para exclusão de linha da tabela
-  // public async removeRow(indexArrayForm: number): Promise<void> {
+  public async removeRow(indexArrayForm: number): Promise<void> {
 
-  //   let arrayDiretivas = this.formData.get('Dados') as FormArray;
+    let arrayDiretivas = this.formData.get('Dados') as FormArray;
+    let retApi = await this.cadastroService.getComponentsAsync(this.returnPathComponent());
+    let componenteUpdate: Componente = this.filterComponente(retApi);
+    //FILTRAR DIRETIVAS CORRESPONDENTE AO COMPONENTE
+    componenteUpdate.Dados = componenteUpdate.Dados.filter(decorator => decorator.Secao == arrayDiretivas.value[indexArrayForm].Secao);
 
-  //   let retApi = await this.cadastroService.getComponentsAsync(this.returnPathComponent());
-  //   let componenteUpdate: Componente = this.filterComponente(retApi);
+    const dialogConfig = new MatDialogConfig();
+    let messageTitleA = componenteUpdate.Dados.length > 1 ? "Tem certeza que deseja remover o item?" : "Tabela com apenas um item, deseja remover tabela?";
 
-  //   //FILTRAR DIRETIVAS CORRESPONDENTE AO COMPONENTE
-  //   componenteUpdate.Dados = componenteUpdate.Dados.filter(decorator => decorator.Secao == arrayDiretivas.value[indexArrayForm].Secao);
-
-  //   const dialogConfig = new MatDialogConfig();
-  //   let messageTitleA = componenteUpdate.Dados.length > 1 ? "Tem certeza que deseja remover o item?" : "Tabela com apenas um item, deseja remover tabela?";
-
-  //   dialogConfig.data = {
-  //     messageTitle: messageTitleA
-  //   };
-  //   const dialogRetorno = this.dialog.open(DialogConfirm, dialogConfig);
-  //   dialogRetorno.afterClosed().subscribe(ret => {
-  //     if (ret) {
-  //       this.cadastroService.getComponents(this.returnPathComponent()).subscribe(response => {
-  //         let componenteUpdate: Componente = this.filterComponente(response);
-  //         this.removeItemFromDecoratorsArray(componenteUpdate, arrayDiretivas.value[indexArrayForm].Id);
-  //         this.cadastroService.putComponents(this.returnPathComponent(), componenteUpdate).subscribe(ret => {
-  //           this.ngOnInit();
-  //           this.buttonEditRow();
-  //         });
-  //       });
-  //     }
-  //   });
-  // }
+    dialogConfig.data = {
+      messageTitle: messageTitleA
+    };
+    const dialogRetorno = this.dialog.open(DialogConfirm, dialogConfig);
+    dialogRetorno.afterClosed().subscribe(ret => {
+      if (ret) {
+        (<FormArray>this.formData.get('Dados')).removeAt(indexArrayForm);
+      }
+    });
+  }
 
   // Metodo Salvar Formulario
   public onSubmit(): void {
@@ -255,15 +233,15 @@ export class DocTableDiretivasComponent implements OnInit {
       this.cadastroService.getComponents(this.returnPathComponent()).subscribe(response => {
         let componente: Componente = this.filterComponente(response);
         componente.Dados = componente.Dados.filter(value => value.Secao != this.sectionType); //ARRAY DE DADOS QUE NÃO PERTENCE A SEÇÃO
-				//HABILITAR CAMPO TEXTAREA DO FORMULARIO PARA PEGAR OS DADOS, POIS SE TIVER DESABILITADO NÃO É POSSIVEL
-				//PEGAR OS DADOS E ATUALIZAR O COMPONENTE
-				//FORMULARIO 'DISABLED' NÃO É POSSIVEL PEGAR OS DADOS
-				this.enableInput();
+        //HABILITAR CAMPO TEXTAREA DO FORMULARIO PARA PEGAR OS DADOS, POIS SE TIVER DESABILITADO NÃO É POSSIVEL
+        //PEGAR OS DADOS E ATUALIZAR O COMPONENTE
+        //FORMULARIO 'DISABLED' NÃO É POSSIVEL PEGAR OS DADOS
+        this.enableInput();
         componente.Dados.push.apply(componente.Dados, this.formData.value.Dados);
-				//BLOQUEANDO FORMULARIO NOVAMENTE
-				this.disableInput();
-        this.cadastroService.editarComponente(this.returnPathComponent(), componente).subscribe(ret => {
-					console.log(2,ret);
+        //BLOQUEANDO FORMULARIO NOVAMENTE
+        this.disableInput();
+        componente.Dados = this.orderDadosById(componente.Dados);
+        this.cadastroService.putComponents(this.returnPathComponent(), componente).subscribe(ret => {
           this.ngOnInit();
           this.buttonEditRow();
         });
@@ -271,5 +249,4 @@ export class DocTableDiretivasComponent implements OnInit {
     }
   }
   //#endregion
-
 }
